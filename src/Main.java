4689350143bf000a36f3f26cfb3fde9772795bd3
Rino Higashionna <e225749@ie.u-ryukyu.ac.jp>
2022-11-01 09:33:18 +0900

    1st commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String args[]){
+        System.out.println("hello");
+    }
+
+    public class Chatbot{
+        Chatbot bot1 = new Chatbot();
+    }
+}
